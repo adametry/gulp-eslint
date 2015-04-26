a4dd44874fc84e1768645cc5aa9f429c8ed69bb5
Adam Reeves <dark.account@gmail.com>
2015-04-25 21:26:03 -0500

    dogfood gulp-eslint via npm test; Simplify gulpfile.js to just use gulp-eslint;

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,11 @@
 var gulp = require('gulp'),
 	eslint = require('./');
 
-gulp.task('throw-on-error', function() {
-	return gulp.src(['**/*.js', '!node_modules/**'])
-		.pipe(eslint())
-		.pipe(eslint.formatEach())
-		.pipe(eslint.failOnError());
-});
-
-gulp.task('throw-after-error', function() {
-	return gulp.src(['**/*.js', '!node_modules/**'])
+gulp.task('test', function() {
+	return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**', '!test/fixtures/**'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
 });
 
-gulp.task('default', function() {
-	return gulp.src(['**/*.js', '!node_modules/**', '!test/fixtures/**'])
-		.pipe(eslint())
-		.pipe(eslint.format());
-});
+gulp.task('default', ['test']);
