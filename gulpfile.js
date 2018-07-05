dd3eab140072a717adf6659accfb2de9e3787c35
刘祺 <gucong@gmail.com>
2018-07-05 13:26:10 +0800

    Update gulp to v4.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@
 const gulp = require('gulp');
 const eslint = require('.');
 
-gulp.task('test', function() {
+function test() {
 	return gulp.src(['**/*.js', '!node_modules/**', '!coverage/**', '!test/fixtures/**'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('default', ['test']);
+module.exports = {
+	test,
+	default: gulp.parallel(test)
+};
